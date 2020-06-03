4edf87d814bf7b3e6d5526bc143fb2dc16007eba
ParitoshChaudhary <paritoshc.at@gmail.com>
2020-06-03 20:32:04 +0530

    Add code for viewing and added a new movie and give star rating to it.

---
+++ api/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render
+from rest_framework import viewsets, status
+from api.models import Movie, Rating
+from api.serializers import MovieSerializer, RatingSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.response import Response
+from rest_framework.decorators import action
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+    authentication_classes = (TokenAuthentication,)
+
+    @action(detail=True, methods=['POST'])
+    def rate_movie(self, request, pk=None):
+        if 'stars' in request.data:
+            movie = Movie.objects.get(id=pk)
+            stars = request.data['stars']
+            user = request.user
+            print(f"Title of the movie is {movie.title} with {stars} stars by {user}")
+
+            try:
+                rating = Rating.objects.get(user=user.id, movie=movie.id)
+                rating.stars = stars
+                rating.save()
+                serilizer = RatingSerializer(rating, many=False)
+                response = {
+                    'message': 'Rating Update',
+                    'result': serilizer.data
+                }
+                return Response(response, status=status.HTTP_200_OK)
+            except:
+                rating = Rating.objects.create(user=user, movie=movie, stars=stars)
+                serilizer = RatingSerializer(rating, many=False)
+                response = {
+                    'message': 'Rating Created',
+                    'result': serilizer.data
+                }
+                return Response(response, status=status.HTTP_200_OK)
+
+        else:
+            response = {
+                'message': 'You need to provide stars'
+            }
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+
+class RatingViewSet(viewsets.ModelViewSet):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
+    authentication_classes = (TokenAuthentication,)
